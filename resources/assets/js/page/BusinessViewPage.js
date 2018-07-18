b24d829e963c5ce0a9e42d13ebdf30a58631e066
jlanir <jacob.lanir@grouphealth.ca>
2018-07-18 16:27:19 -0700

    add some comments to business view page

--- resources/assets/js/page/BusinessViewPage.js
+++ resources/assets/js/page/BusinessViewPage.js
@@ -7,21 +7,46 @@ import withAppBar from "../components/AppBar/WithAppBar";
 import {compose} from "recompose";
 import withPageWrapper from "./withPageWrapper";
 import {withSpacer} from "../components/Spacer";
+import Typography from "@material-ui/core/Typography";
 
 const styles = () => ({});
 
 class BusinessViewPage extends React.Component {
     componentDidMount() {
-        const {getBusiness, match: {params}} = this.props;
-        getBusiness(params.id);
+        const {getBusiness, match} = this.props;
+        getBusiness(match.params.id);
     }
 
+
+    /**
+     * Display the Business
+     * 1.
+     *
+     */
     render() {
         const {businessView} = this.props;
 
+        //replace this with react component
+        if(Object.keys(businessView).length === 0) {
+            return null;
+        }
+
+        const {location, name} = businessView;
+        const {address} = location;
+
         return (
-            <div>
-                {JSON.stringify(businessView)}
+            <div className={"container"}>
+                <Typography component={"p"}>{name}</Typography>
+                <Typography>{address}</Typography>
+                <Typography>
+                    What Do i want to see here?
+                    1. I want to see Business Name, phone, email, and Type,
+                    2. I want to see where its located
+                    3. I want to see what the business has to offer
+                    4. I want to see the Owner name
+                    5. i want to see some images.
+
+                </Typography>
             </div>
         );
     }
