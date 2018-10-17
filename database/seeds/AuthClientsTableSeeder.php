c981946fca98abedf9c7e07341b3bd3b5e04e038
Jacob Balabanov Lanir <jacob@policyreporter.com>
2018-10-17 16:10:11 -0700

    fix load-vars with dev env variable

--- database/seeds/AuthClientsTableSeeder.php
+++ database/seeds/AuthClientsTableSeeder.php
@@ -16,7 +16,7 @@ class AuthClientsTableSeeder extends Seeder
             'name'                   => "Web Client",
             'secret'                 => "FkI9pTCbjIcs4sCpymlHfuBXAKF14O3UGpf0CRAr",
             'revoked'                => 0,
-            'redirect'               => "israelim.ca/redirect",
+            'redirect'               => "http://israelim.ca/redirect",
             'password_client'        => 1,
             'personal_access_client' => 0,
         ]);
